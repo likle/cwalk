7e46e53c89a25d703f0fe31e7a72039da39eb6e9
Leonard Iklé <leonard.ikle@gmail.com>
2018-09-01 17:48:39 +0200

    added banner

--- test/intersection_test.c
+++ test/intersection_test.c
@@ -1,7 +1,18 @@
 #include <cwalk.h>
 #include <stdlib.h>
 
-int intersection_relative()
+int intersection_relative_other()
+{
+  cwk_path_set_style(CWK_STYLE_UNIX);
+
+  if (cwk_path_get_intersection("/test/foo/har", "/test/abc/../foo/bar") != 9) {
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
+
+int intersection_relative_base()
 {
   cwk_path_set_style(CWK_STYLE_UNIX);
 
